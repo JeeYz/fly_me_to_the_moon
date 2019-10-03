427f406dcd0510d560d0a19df5dd63d3b4e06673
JeeYz <Jkdspiegel@gmail.com>
2019-10-03 20:43:29 +0900

    fly me to the moon~!~! :)

---
+++ hello.py
@@ -0,0 +1,14 @@
+# @Author: J.Y.
+# @Date:   2019-10-03T20:42:43+09:00
+# @Last modified by:   J.Y.
+# @Last modified time: 2019-10-03T20:43:06+09:00
+# @License: J.Y. JeeYz
+# @Copyright: J.Y. JeeYz
+
+print('hello, world~!~!')
+
+
+
+
+
+## endl
