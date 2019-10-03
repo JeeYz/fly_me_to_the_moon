5e9896c0d9094ebd0f823300b2f52bc4efd87184
JeeYz <Jkdspiegel@gmail.com>
2019-10-04 01:04:06 +0900

    just play. lol

---
+++ play00.py
@@ -0,0 +1,20 @@
+# @Author: J.Y.
+# @Date:   2019-10-03T20:47:16+09:00
+# @Last modified by:   J.Y.
+# @Last modified time: 2019-10-04T01:03:49+09:00
+# @License: J.Y. JeeYz
+# @Copyright: J.Y. JeeYz
+
+for i in range(10):
+    print(i)
+
+
+for i,j in enumerate(range(10)):
+    print(i, j+1)
+
+
+
+
+
+
+## endl
