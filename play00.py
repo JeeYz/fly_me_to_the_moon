5004c33436bf2d0008f05ecd040aadffd2eebb4a
JeeYz <Jkdspiegel@gmail.com>
2019-10-21 01:53:57 +0900

    update.

--- play00.py
+++ play00.py
@@ -1,13 +1,14 @@
 # @Author: J.Y.
 # @Date:   2019-10-03T20:47:16+09:00
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-10-04T01:03:49+09:00
+# @Last modified time: 2019-10-17T22:06:15+09:00
 # @License: J.Y. JeeYz
 # @Copyright: J.Y. JeeYz
 
 for i in range(10):
     print(i)
 
+print('\n\n')
 
 for i,j in enumerate(range(10)):
     print(i, j+1)
