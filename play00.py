524fba92aa5bb533e8034bf263376f87fb4d2d8f
JeeYz <Jkdspiegel@gmail.com>
2019-11-12 07:28:18 +0900

    cyk algorithm complete~!

--- play00.py
+++ play00.py
@@ -1,7 +1,7 @@
 # @Author: J.Y.
 # @Date:   2019-10-03T20:47:16+09:00
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-10-21T07:31:34+09:00
+# @Last modified time: 2019-11-07T23:34:05+09:00
 # @License: J.Y. JeeYz
 # @Copyright: J.Y. JeeYz
 
@@ -23,7 +23,20 @@ print('\n\n')
 for i, j in enumerate(range(1, 10+1)):
     print(i, '\t', j)
 
+print('\n\n')
 
+for i in range(2, 10):
+    print(i)
+
+print('\n')
+
+for i in range(2, 10+1):
+    print(i)
+
+print('\n')
+
+for i in reversed(range(2, 10+1)):
+    print(i)
 
 
 ## endl
--- play02.py
+++ play02.py
@@ -1,7 +1,7 @@
 # @Author: J.Y.
 # @Date:   2019-10-21T07:31:58+09:00
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-10-21T07:59:50+09:00
+# @Last modified time: 2019-11-07T23:34:11+09:00
 # @License: J.Y. JeeYz
 # @Copyright: J.Y. JeeYz
 
@@ -19,9 +19,18 @@ a = a.tolist()
 print(a)
 print(type(a))
 
+print('\n\n')
 
+b = np.zeros((3, 4))
 
+print(b)
 
+print('\n\n')
+
+c = np.random.rand(10)
+print(c)
+print(type(c))
+print(len(c))
 
 
 ## endl
